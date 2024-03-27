00a3c6959a421864da4c93a621ba8f9bfbd0f1ad
Maria João <75786385+mariajoao722@users.noreply.github.com>
2024-03-27 11:37:59 +0000

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.Kademlia.Node;
+
+import java.sql.SQLOutput;
+import java.sql.Timestamp;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        Node node = new Node(1, 2, String.valueOf(timestamp), "value");
     }
 }
