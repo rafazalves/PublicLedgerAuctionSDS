64f3f328d0b30915d802bd4844d0910115e111c6
monica-a-araujo <up202005209@up.pt>
2024-04-23 15:07:40 +0100

    update main example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         long timestamp = Instant.now().getEpochSecond();
-        Node node = new Node(1, 2, timestamp, "value");
-        Node node2 = new Node(2, 2, "value");
-
+        Node node3 = new Node(2, "value");
     }
 }
