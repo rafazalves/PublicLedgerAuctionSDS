ede31657fe427ac3f7fb89cd6553b2e51d1f5213
monica-a-araujo <up202005209@up.pt>
2024-05-22 21:32:56 +0100

    generate connection and stubs

--- src/main/java/org/gRPC/clientManager.java
+++ src/main/java/org/gRPC/clientManager.java
@@ -1,8 +1,17 @@
 package org.gRPC;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.Kademlia.Node;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
 public class clientManager {
 
     private static clientManager instance;
+
+    private static final Logger logger = Logger.getLogger(serverSetUp.class.getName());
 
 
     public clientManager() {
@@ -12,10 +21,30 @@ public class clientManager {
     public static clientManager getInstance() {
         return instance;
     }
+    private ManagedChannel generateConnection(Node n){
+        return ManagedChannelBuilder
+                .forAddress("localhost", n.getNodePublicPort() )
+                .usePlaintext()
+                .build();
+    }
+
+    public ledgerServiceGrpc.ledgerServiceStub newStub(Node n) throws IOException {
+        final var connection = generateConnection(n);
+        return ledgerServiceGrpc.newStub(connection);
+    }
+
+    public ledgerServiceGrpc.ledgerServiceFutureStub newFutureStub(Node n) throws IOException {
+        final var connection = generateConnection(n);
+
+        return ledgerServiceGrpc.newFutureStub(connection);
+    }
 
     public static void main(String[] args){
-        System.out.println("---------------------------------");
-        System.out.println("-- Cliente -- ");
+
+        logger.info("---------------------------------");
+        logger.info("--          gRPC client        --");
+
+        //ManagedChannel channel = getInstance().generateConnection();
 
 
 
