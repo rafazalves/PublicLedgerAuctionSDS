64e4956853168d6a481f98cf5510ba5ff8b7fddc
monica-a-araujo <up202005209@up.pt>
2024-05-22 20:41:11 +0100

    i dont know

--- src/main/java/org/Kademlia/Client.java
+++ src/main/java/org/Kademlia/Client.java
@@ -1,4 +1,12 @@
 package org.Kademlia;
 
 public class Client {
+
+    public static void main(String[] args){
+        System.out.println("---------------------------------");
+        System.out.println("-- Cliente -- ");
+
+
+
+    }
 }
